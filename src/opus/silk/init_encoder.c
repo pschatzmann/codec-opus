2d75f7dce19dfdf74d9cd63580d30acb5b84b7e2
pschatzmann <phil.schatzmann@gmail.com>
2025-09-06 19:37:12 +0200

    v1.5

--- src/opus/silk/init_encoder.c
+++ src/opus/silk/init_encoder.c
@@ -36,6 +36,10 @@ POSSIBILITY OF SUCH DAMAGE.
 #include "tuning_parameters.h"
 #include "opus/celt/cpu_support.h"
 
+#ifdef ENABLE_DRED
+#include "dred_encoder.h"
+#endif
+
 /*********************************/
 /* Initialize Silk Encoder state */
 /*********************************/
