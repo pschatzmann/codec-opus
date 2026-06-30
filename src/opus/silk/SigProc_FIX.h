dc1920f17c947d4bb3ddc52e99e94a45dd343a19
pschatzmann <phil.schatzmann@gmail.com>
2026-06-30 15:19:47 +0200

    v1.6.1

--- src/opus/silk/SigProc_FIX.h
+++ src/opus/silk/SigProc_FIX.h
@@ -29,10 +29,6 @@ POSSIBILITY OF SUCH DAMAGE.
 #define SILK_SIGPROC_FIX_H
 #include "opus/config.h"
 
-#ifdef  __cplusplus
-extern "C"
-{
-#endif
 
 /*#define silk_MACRO_COUNT */          /* Used to enable WMOPS counting */
 
@@ -601,11 +597,13 @@ static OPUS_INLINE opus_int64 silk_max_64(opus_int64 a, opus_int64 b)
 #define RAND_INCREMENT                      907633515
 #define silk_RAND(seed)                     (silk_MLA_ovflw((RAND_INCREMENT), (seed), (RAND_MULTIPLIER)))
 
-/*  Add some multiplication functions that can be easily mapped to ARM. */
+/*  Add some multiplication functions that can be easily mapped to ARM/MIPS32. */
 
 /*    silk_SMMUL: Signed top word multiply.
           ARMv6        2 instruction cycles.
-          ARMv3M+      3 instruction cycles. use SMULL and ignore LSB registers.(except xM)*/
+          ARMv3M+      3 instruction cycles. use SMULL and ignore LSB registers.(except xM)
+          MIPS32       2 instructions mul+mfhi
+          MIPS32r6     1 instruction muh */
 /*#define silk_SMMUL(a32, b32)                (opus_int32)silk_RSHIFT(silk_SMLAL(silk_SMULWB((a32), (b32)), (a32), silk_RSHIFT_ROUND((b32), 16)), 16)*/
 /* the following seems faster on x86 */
 #define silk_SMMUL(a32, b32)                (opus_int32)silk_RSHIFT64(silk_SMULL((a32), (b32)), 32)
@@ -640,9 +638,5 @@ static OPUS_INLINE opus_int64 silk_max_64(opus_int64 a, opus_int64 b)
 #include "xtensa/SigProc_FIX_lx7.h"
 #endif
 
-
-#ifdef  __cplusplus
-}
-#endif
 
 #endif /* SILK_SIGPROC_FIX_H */
